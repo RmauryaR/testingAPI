718bd9620fffaa1aaf8722fa42ebf9ce964d2ebb
RmauryaR <78673793+RmauryaR@users.noreply.github.com>
2025-12-12 12:10:05 +0530

    Implement CORS support in simpleAPI.js
    Added CORS headers to allow cross-origin requests.

--- simpleAPI.js
+++ simpleAPI.js
@@ -48,9 +48,19 @@ const userData = [
 const server = http.createServer((req, res) => {
   //   res.write("<h1>This is simple API response</h1>");
   //   res.end("Response from simple API completed");
+  // --- Add CORS Headers ---
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    return res.end();
+  }
+  // -------------------------
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.write(JSON.stringify(userData));
-  res.end();
+  res.end(JSON.stringify(userData));
 });
 
 server.listen(4800);
